61a4d45ac4aa77429d5d26a0e6de180ecb4e9445
Daniel Minior <lubie.bataty@gmail.com>
2023-11-28 11:16:42 +0100

    Init commit

---
+++ src/test/java/pl/battacik/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.battacik.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
